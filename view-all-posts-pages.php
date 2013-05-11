ethitter/View-All-Posts-Pages

commit ac45a899644da4439b68da810bf2bdd7efa4d8fe

Allow default settings and assistive text on options page to be fully translated.

view-all-posts-pages.php
@@ -36,22 +36,7 @@ class view_all_posts_pages {
 	private $ns = 'view_all_posts_pages';
 
 	private $settings_key = 'vapp';
-	private $settings_defaults = array(
-		'wlp' => true,
-		'wlp_text' => 'View All',
-		'wlp_class' => 'vapp',
-		'wlp_post_types' => array(
-			'post'
-		),
-		'link' => false,
-		'link_position' => 'below',
-		'link_text' => 'View All',
-		'link_class' => 'vapp',
-		'link_post_types' => array(
-			'post'
-		),
-		'link_priority' => 10
-	);
+	private $settings_defaults = null;
 
 	private $notice_key = 'vapp_admin_notice_dismissed';
 
@@ -148,11 +133,31 @@ class view_all_posts_pages {
 	 * Add rewrite endpoint, which sets query var and rewrite rules.
 	 *
 	 * @global $wp_rewrite
+	 * @uses __
 	 * @uses add_rewrite_endpoint
 	 * @action init
 	 * @return null
 	 */
 	public function action_init() {
+		// Populate default settings, with translation support
+		$this->settings_defaults = array(
+			'wlp'             => true,
+			'wlp_text'        => __( 'View All', 'view_all_posts_pages' ),
+			'wlp_class'       => 'vapp',
+			'wlp_post_types'  => array(
+				'post'
+			),
+			'link'            => false,
+			'link_position'   => 'below',
+			'link_text'       => __( 'View All', 'view_all_posts_pages' ),
+			'link_class'      => 'vapp',
+			'link_post_types' => array(
+				'post'
+			),
+			'link_priority'   => 10,
+		);
+
+		// Register rewrite endpoint, which handles most of our rewrite needs
 		add_rewrite_endpoint( $this->query_var, EP_ALL );
 
 		//Extra rules needed if verbose page rules are requested
@@ -384,13 +389,13 @@ class view_all_posts_pages {
 	 * @return null
 	 */
 	public function action_admin_menu() {
-		add_options_page( __( 'View All Post\'s Pages Options', 'view_all_posts_pages' ), 'View All Post\'s Pages', 'manage_options', $this->ns, array( $this, 'admin_options' ) );
+		add_options_page( __( "View All Post's Pages Options", 'view_all_posts_pages' ), "View All Post's Pages", 'manage_options', $this->ns, array( $this, 'admin_options' ) );
 	}
 
 	/**
 	 * Render options page
 	 *
-	 * @uses settings_fields, this::get_options, this::post_types_array, _e, checked, esc_attr, submit_button
+	 * @uses settings_fields, this::get_options, this::post_types_array, __, _e, checked, esc_attr, submit_button
 	 * @return string
 	 */
 	public function admin_options() {
@@ -406,9 +411,9 @@ class view_all_posts_pages {
 					$post_types = $this->post_types_array();
 				?>
 
-				<h3><em>wp_link_pages</em> Options</h3>
+				<h3><?php printf( __( '%s Options', 'view_all_posts_pages' ), '<em>wp_link_pages</em>' ); ?></h3>
 
-				<p class="description">A "view all" link can be appended to WordPress' standard page navigation using the options below.</p>
+				<p class="description"><?php _e( 'A "view all" link can be appended to WordPress\' standard page navigation using the options below.', 'view_all_posts_pages' ); ?></p>
 
 				<table class="form-table">
 					<tr>
@@ -442,9 +447,9 @@ class view_all_posts_pages {
 					</tr>
 				</table>
 
-				<h3>Standalone Link Options</h3>
+				<h3><?php _e( 'Standalone Link Options', 'view_all_posts_pages' ); ?></h3>
 
-				<p class="description">In addition to appending the "view all" link to WordPress' standard navigation, link(s) can be added above and below post content.</p>
+				<p class="description"><?php _e( 'In addition to appending the "view all" link to WordPress\' standard navigation, link(s) can be added above and below post content.', 'view_all_posts_pages' ); ?></p>
 
 				<table class="form-table">
 					<tr>

commit 7b4f2fac88de3df6da7a83319bef6c37d9b49ac8

Correct translation string problem introduced in ac45a899644da4439b68da810bf2bdd7efa4d8fe.

view-all-posts-pages.php
@@ -4,7 +4,7 @@ Plugin Name: View All Post's Pages
 Plugin URI: http://www.thinkoomph.com/plugins-modules/view-all-posts-pages/
 Description: Provides a "view all" (single page) option for posts, pages, and custom post types paged using WordPress' <a href="http://codex.wordpress.org/Write_Post_SubPanel#Quicktags" target="_blank"><code>&lt;!--nextpage--&gt;</code> Quicktag</a> (multipage posts).
 Author: Erick Hitter & Oomph, Inc.
-Version: 0.8
+Version: 0.8.1
 Author URI: http://www.thinkoomph.com/
 Text Domain: view_all_posts_pages
 
@@ -429,7 +429,7 @@ class view_all_posts_pages {
 	 * @return null
 	 */
 	public function action_admin_menu() {
-		add_options_page( __( "View All Post's Pages Options", 'view_all_posts_pages' ), "View All Post's Pages", 'manage_options', $this->ns, array( $this, 'admin_options' ) );
+		add_options_page( sprintf( __( '%s Options', 'view_all_posts_pages' ), "View All Post's Pages" ), "View All Post's Pages", 'manage_options', $this->ns, array( $this, 'admin_options' ) );
 	}
 
 	/**